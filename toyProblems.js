feb281be4ebd15f99a649bf4e6af2067fa376b0b
Mason Galland <masongalland@users.noreply.github.com>
2017-05-16 11:13:27 -0600

    completed test

--- toyProblems.js
+++ toyProblems.js
@@ -1,27 +1,67 @@
 //JS Basics
 //Run npm test in the command line to test your solutions
 module.exports = {
-  // reverseIt:
+  reverseIt: function(str) {
+    let answer = "";
+
+    for (let i = str.length - 1; i>= 0; i--) {
+      answer += str[i]
+    }
+    return answer;
+  },
 
   //uncomment and finish the reverseIt function. It will take in one parameter which is a String and
   //reverse it
 
-  // removeDups:
+  removeDups: function(arr) {
+    let answer = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      if(answer.indexOf(arr[i]) == -1) {
+        answer.push(arr[i]);
+      }
+    }
+    return answer;
+  },
 
   //uncomment and finish the removeDups function. It will take in one parameter which is an Array
   //remove all duplicates
 
-  // titleIt:
+  titleIt: function(str) {
+    var arr = str.split(' ');
+  
+    for(var i = 0; i < arr.length; i++){
+      var upper = arr[i][0].toUpperCase();
+      arr[i] = upper + arr[i].slice(1);
+    }
+    return arr.join(' ');
+  },
 
   //uncomment and finish the titleIt function. It will take in one parameter which is a String and
   //capitalize the first letter of each word
 
-  // vowelCounter:
+  vowelCounter: function(str) {
+    let allVowels = str.match(/[aeiou]/gi)
+
+    if (allVowels !== null) {
+      return allVowels.length;
+    }
+    else {
+      return 0;
+    }
+  },
 
   //uncomment and finish the vowelCounter function. It will take in one parameter which is a String and
   //return the number of vowels in the string
 
-  // isPrime:
+  isPrime: function(num) {
+    for(let i = 2; i < num; i++) {
+      if(num % i === 0) {
+        return false;
+      }
+    }
+    return num > 1;
+  },
 
   //uncomment and finish the isPrime function. It will take in one parameter which is a Number and
   //return true if it is prime and false if it is not
@@ -30,7 +70,7 @@ module.exports = {
   //var foo = 10 + '20';
   //uncomment the foo property and give your answer as its value
 
-  //foo:
+  foo: '1020',
 
   //what is the outcome of the two console.logs below?
   //   var foo = "Hello";
@@ -41,6 +81,6 @@ module.exports = {
   // console.log(foo + bar);
   // uncomment the log1 and log2 properties and give your answers as their values
 
-  // log1:
-  // log2:
+  log1: 'Hello World',
+  log2: undefined
 }
